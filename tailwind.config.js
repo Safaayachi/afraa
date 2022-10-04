dd831288d69ba4c1e82b29b3e4c5f467ee858359
safa ayachi <safa.ayachi@polytechnicien.tn>
2022-10-04 18:46:45 +0100

    font ready

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [ "./pages/**/*.{js,ts,jsx,tsx}",
-  "./components/**/*.{js,ts,jsx,tsx}",],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        cairo: ["Cairo", "sans-serif"],
+      },
+      colors: {
+        primary: "#b38e55",
+        basic: "#f7f7f7",
+        secondary: "#132d46",
+        shade: "#e7e9ec",
+        success: "#00800a",
+        warning: "#f6b12d",
+        danger: "#d11317",
+        tint: "#ffffff",
+        darkTint: "#babec0",
+      },
+    },
   },
   plugins: [],
-}
+};
